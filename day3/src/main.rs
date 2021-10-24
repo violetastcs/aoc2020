e98a470d12f2fa025d84fe9faa0183bcf83ec3fb
xcafebabe <xcafebabe@riseup.net>
2021-10-24 21:03:19 +0100

    finish day 3

--- day3/src/main.rs
+++ day3/src/main.rs
@@ -1,3 +1,31 @@
+const INPUT: &str = include_str!("input.txt");
+
+fn calc_slope((dx, dy): (usize, usize)) -> usize {
+        INPUT
+                .lines()
+                .map(|l| l.chars().cycle())
+                .enumerate()
+                .step_by(dy)
+                .filter_map(|(i, mut p)| p.nth((i / dy) * dx))
+                .filter(|c| *c == '#')
+                .count()
+}
+
 fn main() {
-        println!("Hello, world!");
+        println!("-- PART 1 --");
+
+        let count = calc_slope((3, 1));
+        
+        println!("Answer: {}", count);
+
+        println!("-- PART 2 --");
+
+        let slopes = [(1,1), (3,1), (5,1), (7,1), (1,2)];
+
+        let product: usize = slopes
+                .into_iter()
+                .map(calc_slope)
+                .product();
+
+        println!("Answer: {}", product);
 }
