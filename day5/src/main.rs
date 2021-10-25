3b43b3b62bedbc93eeabe01ca004eeeff448b099
xcafebabe <xcafebabe@riseup.net>
2021-10-25 23:27:45 +0100

    finish day 5

--- day5/src/main.rs
+++ day5/src/main.rs
@@ -1,9 +1,14 @@
+#![feature(saturating_int_impl)]
+
 const INPUT: &str = include_str!("input.txt");
+
+use std::collections::HashSet;
+use std::num::Saturating;
 
 fn main() {
         println!("-- PART 1 --");
 
-        let mut highest_id = INPUT 
+        let highest_id = INPUT 
                 .lines()
                 .map(|l| (&l[0..7], &l[7..]))
                 .map(|(r, c)| {
@@ -53,4 +58,68 @@ fn main() {
                 .unwrap();
 
         println!("Answer: {}", highest_id);
+
+        println!("-- PART 2 --");
+
+        let mut seat_ids: HashSet<_> = INPUT 
+                .lines()
+                .map(|l| (&l[0..7], &l[7..]))
+                .map(|(r, c)| {
+                        let r = r.chars();
+                        let mut space = (0, 127);
+
+                        for half in r {
+                                let size = space.1 - space.0;
+
+                                match half {
+                                        // lower
+                                        'F' => {
+                                                space.1 = space.0 + (size / 2);
+                                        },
+                                        // upper
+                                        'B' => space.0 = space.1 - (size / 2),
+
+                                        _ => unimplemented!()
+                                }
+                        }
+
+                        (space.0, c)
+                })
+                .map(|(r, c)| {
+                        let c = c.chars();
+                        let mut space = (0, 7);
+
+                        for half in c {
+                                let size = space.1 - space.0;
+
+                                match half {
+                                        // lower
+                                        'L' => {
+                                                space.1 = space.0 + (size / 2);
+                                        },
+                                        // upper
+                                        'R' => space.0 = space.1 - (size / 2),
+
+                                        _ => unimplemented!()
+                                }
+                        }
+
+                        (r, space.0)
+                })
+                .map(|(r, c)| r * 8 + c)
+                .collect();
+
+        let max = *seat_ids.iter().max().unwrap();
+        let min = *seat_ids.iter().min().unwrap();
+
+        for seat in min..max {
+                match (seat_ids.contains(&(seat - 1)),  seat_ids.contains(&seat), seat_ids.contains(&(seat + 1))) {
+                        (true, false, true) => { 
+                                println!("Answer: {}", seat);
+                                break
+                        },
+
+                        _ => continue
+                }
+        }
 }
