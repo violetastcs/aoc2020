5efee419850a4187e99d21f7ad38b3204dc21d5e
xcafebabe <xcafebabe@riseup.net>
2021-10-25 23:59:47 +0100

    revert

--- day6/src/main.rs
+++ day6/src/main.rs
@@ -25,8 +25,8 @@ fn main() {
                         .map(|lines| -> HashSet<char> { lines
                                 .chars()
                                 .collect() })
-                        .reduce(|acc, set| -> HashSet<char> { (&acc & &set)
-                                .iter()
+                        .reduce(|acc, set| -> HashSet<char> { acc
+                                .intersection(&set)
                                 .map(|c| *c)
                                 .collect() })
                         .unwrap()
